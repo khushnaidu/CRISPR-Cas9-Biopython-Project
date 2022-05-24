d5110356a7298636b75010f8d331bf98992d8679
Evan Chopra <evanchopra@MacBook-Pro-2.local>
2022-05-23 22:00:32 -1000

    all cases working

--- step1-4.py
+++ step1-4.py
@@ -7,35 +7,56 @@ genome_list = []
 
 # Opens the file that has the test cases in it.
 # Reads in each genome into its own list, per line
-with open('test.fna') as f:
-    line = f.readline()
-    all_genome = ""
+# BROKEN
+with open('gene.fna') as f:
+    line = f.readline().rstrip("\n")
+    all_genome = []
+    counter = 0
     while line:
-        if line.find(">") != -1:
-            genome_list.append(all_genome)
-            all_genome = ""
-            line = f.readline()
-        else:
-            line = f.readline().rstrip("\n")
-            all_genome += line
-    genome_list.append(all_genome)
-genome_list = [x for x in genome_list if x]
+        if(line != ""):
+            ## This is the line we want to record as the genome
+            if line.find(">") == -1:
+                all_genome.append(line)
+                line = f.readline().rstrip("\n")
+            else:
+                counter += 1
+                g = ''.join(all_genome)
+                genome_list.append(g)
+                all_genome = []
+                line = f.readline().rstrip("\n")
+    g = ''.join(all_genome)
+    genome_list.append(g)
 
+# Get rid of extra entries that are ''
+genome_list = [x for x in genome_list if x]
 # Now gpo through each list genome 
 # and run algorthim to pick out sequnces of 20 nucleotides preceding a PAM.
 all_genomes_RGNA = []
 for a_genome in genome_list:
     all_GRNA_sequnces_current = []
     find_index = 0
-    while find_index is not -1:
+    while find_index != -1:
         find_index = a_genome.find("NGG")
-        print(find_index)
         if (find_index > 20):
             all_GRNA_sequnces_current.append(a_genome[find_index-20:find_index])
             a_genome = a_genome[find_index+3:]
-            print(a_genome)
-            print(find_index)
     all_genomes_RGNA.append(all_GRNA_sequnces_current)
+
+# Convert to complement
+for seq in range(len(all_genomes_RGNA)):
+    for gRNA in range(len(all_genomes_RGNA[seq])):
+        new_string = ""
+        for n in all_genomes_RGNA[seq][gRNA]:
+            if(n == "A"):
+                new_string += "T"
+            if(n == "T"):
+                new_string += "A"
+            if(n == "G"):
+                new_string += "C"
+            if(n == "C"):
+                new_string += "G"
+        all_genomes_RGNA[seq][gRNA] = new_string
+    new_string = ""
 
 print(all_genomes_RGNA)
                         
